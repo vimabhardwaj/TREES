fe7aed9bb099519e502018376204b4c94f014f17
vimabhardwaj <82300531+vimabhardwaj@users.noreply.github.com>
2022-05-06 00:15:05 +0530

    longest bloodline of binarytree.cpp

---
+++ longest bloodline of a tree.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+public:
+
+   void solve(Node* root, int sum, int &maxSum, int len, int &maxlen){
+       // base case
+       if(root==NULL){
+           if(len>maxlen){
+           maxlen= len;
+           maxSum= sum;
+           }
+           
+           else if(len==maxlen){
+           maxSum= max(sum,maxSum); 
+           }
+        
+        return;    
+       }
+       sum= sum+root->data;
+       solve(root->left,sum,maxSum,len+1,maxlen);
+       solve(root->right,sum,maxSum,len+1,maxlen);
+   }
+    
+    int sumOfLongRootToLeafPath(Node *root)
+    {
+        int len=0;
+        int maxlen=0;
+        int sum=0;
+        int maxSum= INT_MIN;
+        solve(root,sum,maxSum,len,maxlen);
+        return maxSum;
+    }
+};
